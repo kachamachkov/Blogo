05a42824caf7847f81f2320d1a4ee1301b1bdb22
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-14 17:01:56 +0300

    Render posts dynamically

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,32 +1,18 @@
 const router = require('express').Router();
 const Post = require('../models/Post');
 
-router.get('', (req, res) => {
-  res.render('index');
+router.get('', async (req, res) => {
+
+  try {
+    const data = await Post.find();
+    res.render('index', { data });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 
-function insertPostData() {
-  Post.insertMany([
-    {
-      title: "Test title",
-      body: "This is the test body"
-    },
-    {
-      title: "Test title2",
-      body: "This is the test body2"
-    },
-    {
-      title: "Test title3",
-      body: "This is the test body3"
-    },
-    {
-      title: "Test title4",
-      body: "This is the test body4"
-    },
-  ]);
-}
-insertPostData();
+
 
 router.get('/about', (req, res) => {
   res.render('about');
