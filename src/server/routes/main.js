3bfc1dd2b6011a67e5bbd878bbc6090519286e87
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-14 16:51:02 +0300

    Add test seed data to DB

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -1,11 +1,35 @@
 const router = require('express').Router();
+const Post = require('../models/Post');
 
 router.get('', (req, res) => {
-  res.render('index')
+  res.render('index');
 });
 
-router.get('/about', (req,res)=>{
-  res.render('about')
-})
+
+function insertPostData() {
+  Post.insertMany([
+    {
+      title: "Test title",
+      body: "This is the test body"
+    },
+    {
+      title: "Test title2",
+      body: "This is the test body2"
+    },
+    {
+      title: "Test title3",
+      body: "This is the test body3"
+    },
+    {
+      title: "Test title4",
+      body: "This is the test body4"
+    },
+  ]);
+}
+insertPostData();
+
+router.get('/about', (req, res) => {
+  res.render('about');
+});
 
 module.exports = router;
