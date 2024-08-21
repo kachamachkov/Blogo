d460900188e65cb8c913b7a56d6fcbbd86619f6c
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-21 21:00:33 +0300

    Add blog main picture

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -3,19 +3,25 @@ const Post = require('../models/Post');
 
 router.get('', async (req, res) => {
 
-  try {
-    const data = await Post.find();
-    res.render('index', { data });
-  } catch (error) {
-    console.log(error);
-  }
+    const locals = {
+        title: 'NodeJS Blogo',
+        description: 'Simple blog created with NodeJS, Express, MongoDB'
+    };
+
+    try {
+        const data = await Post.find();
+
+        res.render('index', { locals, data });
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 
 
 
 router.get('/about', (req, res) => {
-  res.render('about');
+    res.render('about');
 });
 
 module.exports = router;
