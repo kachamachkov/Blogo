19da5b11eea26770e2c036d78a3280d69b966589
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-23 16:06:24 +0300

    Add login functionality

--- src/server/routes/admin.js
+++ src/server/routes/admin.js
@@ -22,55 +22,35 @@ router.get('/admin', (req, res) => {
     }
 });
 
-// router.post('/admin', async (req, res) => {
-//     try {
-//         const { username, password } = req.body;
-//         const user = await User.findOne({ username });
-
-//         if (!user) {
-//             return res.status(401).json({ message: 'Invalid credentials' });
-//         }
-
-//         const isPasswordValid = await bcrypt.compare(password, user.password);
-
-//         if (!isPasswordValid) {
-//             return res.status(401).json({ message: 'Invalid credentials' });
-//         }
-
-//         const token = jwt.sign({ userId: user._id }, jwtSecret);
-//         res.cookie('token', token, { httpOnly: true });
-
-
-//         res.redirect('/dashboard');
-
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-
-// });
-
-
 router.post('/admin', async (req, res) => {
     try {
         const { username, password } = req.body;
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.findOne({ username });
 
-        try {
-            const user = await User.create({ username, password: hashedPassword });
-            res.status(201).json({ message: 'User created successfully', user });
-        } catch (error) {
-            if (error) {
-                if (error.code === 11000) {
-                    res.status(409).json({ message: 'User already in use' });
-                }
-                res.status(500).json({ message: 'Internal server error' });
-            }
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid credentials' });
         }
 
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign({ userId: user._id }, jwtSecret);
+        res.cookie('token', token, { httpOnly: true });
+
+
+        res.redirect('/dashboard');
+
     } catch (error) {
-        console.log(error)
+        console.log(error.message);
     }
+
 });
+
+
+
 
 router.get('/dashboard', async (req, res) => {
     res.render('admin/dashboard');
@@ -78,3 +58,26 @@ router.get('/dashboard', async (req, res) => {
 
 
 module.exports = router;
+
+
+// router.post('/admin', async (req, res) => {
+//     try {
+//         const { username, password } = req.body;
+//         const hashedPassword = await bcrypt.hash(password, 10);
+
+//         try {
+//             const user = await User.create({ username, password: hashedPassword });
+//             res.status(201).json({ message: 'User created successfully', user });
+//         } catch (error) {
+//             if (error) {
+//                 if (error.code === 11000) {
+//                     res.status(409).json({ message: 'User already in use' });
+//                 }
+//                 res.status(500).json({ message: 'Internal server error' });
+//             }
+//         }
+
+//     } catch (error) {
+//         console.log(error)
+//     }
+// });
