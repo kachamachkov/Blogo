2f383b015a4e0134cef23b62d922f22a08f6d78d
kachamachkov <kachamachkovboyan@gmail.com>
2024-08-23 13:26:12 +0300

    Add pagination -  logic and render

--- src/server/routes/main.js
+++ src/server/routes/main.js
@@ -9,10 +9,28 @@ router.get('', async (req, res) => {
     };
 
     try {
-        const data = await Post.find();
 
-        res.render('index', { locals, data });
-        
+        let perPage = 3;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([{ $sort: { createdAt: -1 } }])
+            .skip(perPage * page - perPage)
+            .limit(perPage)
+            .exec();
+
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage)
+
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null,
+        })
+
     } catch (error) {
         console.log(error);
     }
